73ca569a2cdcce55afe238b9362ba72b43a16c04
beth-em <beth.murillo318@gmail.com>
2025-01-30 19:50:37 -0700

    updates

---
+++ Develop/index.js
@@ -0,0 +1,72 @@
+// TODO: Include packages needed for this application
+
+import inquirer from 'inquirer';
+import fs from 'fs';
+import generateMarkdown from './utils/generateMarkdown.js';
+
+// TODO: Create an array of questions for user input
+const questions = [ {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of you project?',
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'Provide a short description of your project:',
+},
+{
+    type: 'input',
+    name: 'installation',
+    message: 'What are the installation instructions?',
+},
+{
+    type: 'input',
+    name: 'usage',
+    message: 'What is the usage information?',
+},
+{
+    type: 'input',
+    name: 'license',
+    message: 'Choose a license for your project:',
+    choices: ['MIT', 'Apache 2.0', 'GNU v3.0', 'BSD 3-Clause', 'BSD 2-Clause', 'Boost Software License 1.0', 'None'],
+},
+{
+    type: 'input',
+    name: 'contributing',
+    message: 'What are the guidelines for contributing?',
+},
+{
+    type: 'input',
+    name: 'tests',
+    message: 'What are the test instructions?',
+},
+{
+    type: 'input',
+    name: 'GitHub',
+    message: 'What is your GitHub username?:',
+},
+{
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+},
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => 
+        err ? console.error(err) : console.log('README.md generated!')
+    );
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        const markdown = generateMarkdown(answers);
+        writeToFile('README.md', markdown);
+    });
+}
+
+// Function call to initialize app
+init();
---
+++ Develop/utils/generateMarkdown.js
@@ -0,0 +1,67 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+    if (!license) return '';
+    return `![License Badge](https://img.shields.io/badge/license-${license}-blue)`;
+}
+
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+    if (!license) return '';
+    return `- [License Link](http://opensource.org/licenses/${license})`;
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+    if (!license) return '';
+    return `## License
+This project is licensed under the ${license} license.`;
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+    const license = data.license;
+
+    const licenseLink = renderLicenseLink(license);
+    const licenseBadge = renderlicenseBadge(license);
+
+    return `# ${data.title}
+
+${renderLicenseBadge(data.license)}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+${renderLicenseLink(data.license)}
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation'
+${data.installation}
+
+## Usage
+${data.usage}
+
+${renderlicenseSection(data.license)}
+
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+If you have any questions, feel free to reach out:
+
+- Github: [${data.github}](https://github.com/${data.github})
+- Email: [${data.email}](mailto:${data.email})
+`;
+}
+
+export default generateMarkdown;
